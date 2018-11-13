d759b37af7054085e81316c6d9aabc454755ae90
hbot21 <43694980+hbot21@users.noreply.github.com>
2018-11-13 06:41:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -880,6 +880,39 @@ setInterval(function(){})
     }
 });
 
+client.on('message', message => {
+	var prefix = "#"
+  if (message.author.x5bz) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "ban") {
+               if(!message.channel.guild) return message.reply('** This command only for servers**');
+         
+  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**You Don't Have ` BAN_MEMBERS ` Permission**");
+  if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
+  let user = message.mentions.users.first();
+  let reason = message.content.split(" ").slice(2).join(" ");
+  if (message.mentions.users.size < 1) return message.channel.send({
+  files: [
+    "./images/met.png"
+  ]
+})
+  
+  if (!message.guild.member(user)
+  .bannable) return message.reply("**I D'ont Have Permission For Ban This User**");
+
+  message.guild.member(user).ban(7, user);
+
+
+  message.channel.send(`:white_check_mark:  ${user.tag} banned from the server ! :airplane:  `)
+}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
 
