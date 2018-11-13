5a5e1feff5e1872bce938fa320ad3625c26860fa
hbot21 <43694980+hbot21@users.noreply.github.com>
2018-11-13 05:43:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -137,6 +137,21 @@ client.on("guildMemberAdd", member => {
       }
       });
 
+
+client.on('message', message => {
+    if (message.content === "$bot") {
+           if(!message.channel.guild) return message.reply('** This command only for servers **');
+    let embed = new Discord.RichEmbed()
+ .setColor('RANDOM')
+ .addField("**اسم السيرفر**", message.guild.name)
+ .addField("**عدد السيرفرات الي فيها البوت:**" , client.guilds.size)
+ .addField("**المستخدمين:**", client.users.size)
+ .addField("**قنوات:**", client.channels.size)
+message.channel.sendEmbed(embed);
+   }
+});
+
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
 
